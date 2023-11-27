5cd6aadc9bcf0333f3b88669ceb694c6f8ee9c9e
David Izhak <itskovdi@gmail.com>
2023-11-27 20:14:41 +0200

    Added deployments manifest files.

--- DockerImg/index.php
+++ DockerImg/index.php
@@ -2,6 +2,6 @@
 $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello From Kubernetes</h1><br>";
-echo "Server IP Address is: $ip_server", "<br><p>";
-echo "Made by <font color=blue>David Izhak";
+echo "Server IP Address is: $ip_server", "<br>";
+echo "Made by <p style=\"color:#663399\">David Izhak</p>";
 ?>
